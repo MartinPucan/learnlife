62923800fae0b3d4b50199a6934181ef24802459
Martin Pucan <martin.pucan@pilulka.cz>
2020-01-03 02:07:14 +0100

    rewrite nav.php

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,15 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+    
     <img src="/img/open-book.png" width="30" height="30" class="d-inline-block align-top" alt="book learnlife">
         <a class="navbar-brand px-4" href="/">LearnLife</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/php">PHP <span class="sr-only">(current)</span></a>
-          </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/php">PHP <span class="sr-only">(current)</span></a>
+            </li>
             <li class="nav-item">
               <a class="nav-link" href="/operating-system">OS</a>
             </li>
@@ -19,9 +20,9 @@
             <li class="nav-item">
               <a class="nav-link" href="/english">EN</a>
             </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/articles">Articles</a>
-              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/articles">Articles</a>
+            </li>
       </ul>
 
         <form action="search.php" method="post" class="form-inline my-2 my-lg-0">
